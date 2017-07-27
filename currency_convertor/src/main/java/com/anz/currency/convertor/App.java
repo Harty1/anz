5912558b976af8d0f566314cb4069212a166a507
Harty1 <tyagi.harshvardhan@gmail.com>
2017-07-27 19:09:52 +0530

    Project created

---
+++ currency_convertor/src/main/java/com/anz/currency/convertor/App.java
@@ -0,0 +1,13 @@
+package com.anz.currency.convertor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
